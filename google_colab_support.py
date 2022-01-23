5ebfc1f3e3fe06fc9fe33a8e414e7a9298457fcb
vutrian576 <vutrian576@gmail.com>
2022-01-23 23:14:20 +0700

    ok

--- google_colab_support.py
+++ google_colab_support.py
@@ -4,7 +4,7 @@ def get_database():
     import pymongo
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING_MGA1 = "mongodb://mnetwordtest2021:mnetwordtest2021@94.237.76.25:27017/admin?retryWrites=true&w=majority"
+    CONNECTION_STRING_MGA1 = "mongodb://mdata_admin:kalilinux2018@23.172.112.93:27017/admin?retryWrites=true&w=majority"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     from pymongo import MongoClient

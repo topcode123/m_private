658c8db67aef4eabf00f86a86d71e2dde42e8a8c
cuong <cuong.hoangduc@cxview.ai>
2022-12-31 14:39:10 +0000

    update spin

--- google_colab.py
+++ google_colab.py
@@ -46,7 +46,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',  # This is another valid field
 }
 def ColabSimple():
-      print(CONNECTION_STRING_MGA1)
       if cl.count_documents({})>0:
         try:
           # h=h+1
